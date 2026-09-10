2794fcca46304fe6fd1d4c0613b0ac2dea6d81f1
Kairung Vangmanaw <94585323+VangmanawKairung@users.noreply.github.com>
2026-09-10 20:00:30 +0700

    style: remove design overlay

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,9 @@ import './App.css'
 import Card from './components/Card'
 import Footer from './components/Footer'
 
-import desktopDesign from '../.reference/design/desktop-design.jpg';
-import mobileDesign from '../.reference/design/mobile-design.jpg';
-
 function App() {
   return (
     <>
-      <picture id='design-overlay'>
-      <source media='(min-width: 1025px)' srcSet={desktopDesign} />
-      <img src={mobileDesign} alt='Design reference' />
-      </picture>
-
       <main className="main-content">
         <Card />
       </main>
